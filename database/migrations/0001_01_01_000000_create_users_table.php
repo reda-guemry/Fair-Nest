941f3d233b9f963444723edce15a8eee3ce0dd84
reda-guemry <guemryreda@gmail.com>
2026-02-23 17:59:36 +0000

    create the migratiosn of tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('reputation')->default(0) ; 
+            $table->boolean('status')->default(1) ;
+            $table->text('reason')->nullable() ; 
             $table->rememberToken();
             $table->timestamps();
         });
