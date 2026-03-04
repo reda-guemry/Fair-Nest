0544144da9a8ff4866758e19e48a122ad700562c
reda-guemry <guemryreda@gmail.com>
2026-03-04 03:08:42 +0000

    conflis rest

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,12 +1,6 @@
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
-<<<<<<< HEAD
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: 'reverb',
-=======
 
 
 window.Pusher = Pusher;
@@ -15,15 +9,10 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',  
->>>>>>> feature/Message
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> feature/Message
